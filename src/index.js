51514edcda2e7023e5e8ce54ea2a653cff19960c
Svetlana.Safonova <safonova.sv.a@gmail.com>
2022-06-28 22:28:57 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let modN = String(n > 0 ? n : -n);
+  let newN = '';
+  for(let i = modN.length - 1; i >= 0; i--) {
+    newN += modN[i];
+  }
+  return +newN;
 }
